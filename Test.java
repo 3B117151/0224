524aac9b9d78daa8e1012b0ffba7bbd492faac90
3B117151 <114204306+3B117151@users.noreply.github.com>
2023-02-24 10:55:12 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+	public static void main (String[] args){
+		int [] data =new int[3];
+		data[0]=1;
+		System.out.println(data[0]);
+	}
+}
+
+class Human{
+	int sex;
+	int height;
+	int weight;
+}
---
+++ Test1.java
@@ -0,0 +1,19 @@
+public class Test1{
+	public static void main (String[] args){
+		System.out.println("hello");
+	}
+}
+
+class Human{
+	int sex;
+	int height;
+	int weight;
+	
+	public void createVariable(){
+		int a=0;
+	}
+	
+	public void printHight2(){
+		System.out.println(a);
+	}
+}
